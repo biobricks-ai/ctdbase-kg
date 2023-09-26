6e3ab306632bae0a97c05054fce901c8e698da16
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-09-26 17:07:59 -0400

    Add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "CTDbase-kg BioBrick";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    hdt-cpp = {
+      url = "github:insilica/nix-hdt";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    morph-kgc = {
+      url = "github:insilica/nix-morph-kgc";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, hdt-cpp, morph-kgc }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell {
+          buildInputs = [
+            hdt-cpp.packages.${system}.default
+            morph-kgc.packages.${system}.default
+          ];
+        };
+      });
+}
