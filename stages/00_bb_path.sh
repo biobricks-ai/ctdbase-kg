c71fd8bfcf21ecb48374c268d0e0c861f9c854bc
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-09-26 20:57:34 -0400

    Add initial pipeline

---
+++ stages/00_bb_path.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Script to prepare data
+
+# Get local [ath]
+localpath=$(pwd)
+echo "Local path: $localpath"
+
+# Create the list directory to save list of remote files and directories
+datapath="$localpath/data"
+echo "Data path: $datapath"
+mkdir -p $datapath
+cd $datapath;
+
+# Define brick names to process
+brick_names='ctdbase'
+
+export datapath
+echo $brick_names | xargs -n1 python3 -c 'import sys; from biobricks.brick import Brick; b = Brick.Resolve(sys.argv[1]); print( "|".join([ "PATH_SENTINEL", str(b.path() / "brick"), sys.argv[1], b.url() ] ))' \
+  | perl -F'\|' -npe '
+  if( $F[0] eq "PATH_SENTINEL" ) {
+    shift @F;
+    my ($path, $name, $url) = @F;
+    symlink($path, "$ENV{datapath}/$name");
+    $_ = $url;
+  } else { $_ = "" }' | sort > $datapath/brick-manifest
---
+++ stages/01_rml.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Script to create N-Triples graph using RML
+
+# Get local path
+localpath=$(pwd)
+echo "Local path: $localpath"
+
+# Create raw directory
+rawpath="$localpath/raw"
+mkdir -p $rawpath
+echo "Raw path: $rawpath"
+
+# Process RML mapping
+python3 -m morph_kgc stages/data-ctdbase.ini
+
+# Sort in-place for reproducible hash
+sort -o $rawpath/knowledge-graph.nt $rawpath/knowledge-graph.nt 
