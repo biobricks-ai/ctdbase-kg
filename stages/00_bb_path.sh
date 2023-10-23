7a9088c8cdda6f6defc5f3a1431fd7180104ffae
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-10-23 12:27:43 -0400

    Use biobrick-setup-source script

--- stages/00_bb_path.sh
+++ stages/00_bb_path.sh
@@ -6,6 +6,8 @@
 localpath=$(pwd)
 echo "Local path: $localpath"
 
+eval $( $localpath/vendor/biobricks-script-lib/activate.sh )
+
 # Create the list directory to save list of remote files and directories
 datapath="$localpath/data-source"
 echo "Data path: $datapath"
@@ -15,12 +17,4 @@ cd $datapath;
 # Define brick names to process
 brick_names='ctdbase'
 
-export datapath
-echo $brick_names | xargs -n1 python3 -c 'import sys; from biobricks.brick import Brick; b = Brick.Resolve(sys.argv[1]); print( "|".join([ "PATH_SENTINEL", str(b.path() / "brick"), sys.argv[1], b.url() ] ))' \
-  | perl -F'\|' -npe '
-  if( $F[0] eq "PATH_SENTINEL" ) {
-    shift @F;
-    my ($path, $name, $url) = @F;
-    symlink($path, "$ENV{datapath}/$name");
-    $_ = $url;
-  } else { $_ = "" }' | sort > $datapath/brick-manifest
+biobrick-setup-source --output-dir $datapath $brick_names
