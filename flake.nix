11bb4f4653dd2691826a03501cdcb0638f899f9c
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-10-23 09:33:22 -0400

    Use dev-shell flake

--- flake.nix
+++ flake.nix
@@ -1,29 +1,9 @@
 {
   description = "CTDbase-kg BioBrick";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    flake-utils.url = "github:numtide/flake-utils";
-    hdt-cpp = {
-      url = "github:insilica/nix-hdt";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    morph-kgc = {
-      url = "github:insilica/nix-morph-kgc";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs = { dev-shell.url = "github:biobricks-ai/dev-shell"; };
 
-  outputs = { self, nixpkgs, flake-utils, hdt-cpp, morph-kgc }:
-    flake-utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; }; {
-        devShells.default = mkShell {
-          buildInputs = [
-            hdt-cpp.packages.${system}.default
-            morph-kgc.packages.${system}.default
-          ];
-        };
-      });
+  outputs = { self, dev-shell }: {
+    devShells = dev-shell.devShells;
+  };
 }
